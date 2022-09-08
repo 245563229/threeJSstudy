a2bc3ed53a9204f96b99e99365411479205db5ec
“gefanghe” <aa208208@qq.com>
2022-09-08 15:42:00 +0800

    Gsap动画库,clock跟踪时间处理动画

--- my-vue-app/src/main06.js
+++ my-vue-app/src/main06.js
@@ -1,4 +1,4 @@
-//优化requestAnimationFrame
+//clock跟踪时间处理动画
 import { createApp } from "vue";
 import "./style.css";
 import * as THREE from "three";
@@ -35,18 +35,25 @@ scene.add(cube);
 //
 document.body.appendChild(renderer.domElement);
 // renderer.render(scene, camera);
-
+//添加坐标轴服务器
+const axesHelper = new THREE.AxesHelper(5);
+scene.add(axesHelper);
+//设置时钟
+const clock = new THREE.Clock();
 //创建控制器
 const controls = new OrbitControls(camera, renderer.domElement);
 camera.position.set(0, 20, 10);
 controls.update();
 function animate(time) {
+  //获取时钟时长
+  // let clockTime = clock.getElapsedTime();
+  //获取两次时钟间隔时间
+  let deltaTime = clock.getDelta();
+  // console.log("时钟运行时时长", clockTime);
+  console.log("两次获取间隔时间", deltaTime);
   //默认传入time,单位是毫秒
   //下一帧再执行animate
   // cube.position.x -= 0.01;
-  // if (cube.position.x < -5) {
-  //   cube.position.x = 5;
-  // }
   // 优化方案
   let t = (time / 1000) % 5;
   cube.position.x = t * 1;
@@ -61,8 +68,5 @@ function animate(time) {
   renderer.render(scene, camera);
 }
 animate();
-//添加坐标轴服务器
-const axesHelper = new THREE.AxesHelper(5);
-scene.add(axesHelper);
 
 createApp(App).mount("#app");
